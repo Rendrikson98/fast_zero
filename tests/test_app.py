0d69432a49fc9257c23fede54770759ee058a6ee
rendrikson <rendrikson.soares@lccv.ufal.br>
2025-01-10 17:24:36 -0300

    aprendendo a definir o schema do pydantic

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,12 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello, World!'}
+
+
+def test_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert '<h1>Hello, World!</h1>' in response.text
